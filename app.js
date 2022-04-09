7c4785a4a29a3598511ab826775376092e880734
Lucas Froidefond <froidefond.lucas@gmail.com>
2022-04-09 22:36:51 +0200

    gteé

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 var timeline = new TimelineMax();
 
 timeline.to(".menu", 1.5, {left: '0%',ease: Expo.easeInOut, delay: -1.5});
-timeline.staggerFrom(".menu ul li", 0.4, {x: -100, opacity: 0}, 0.1);
+timeline.staggerFrom(".menu ul li ", 0.4, {x: -100, opacity: 0}, 0.1);
 
 timeline.reverse();
 
